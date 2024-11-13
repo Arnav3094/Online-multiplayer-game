b204bb50b97e6ddb9a6e48796618b705faf62fd3
Arnav3094 <arnav3094pvt@gmail.com>
2024-11-13 11:42:35 +0530

    refactor and add logging

--- app/src/main/java/androidsamples/java/tictactoe/MainActivity.java
+++ app/src/main/java/androidsamples/java/tictactoe/MainActivity.java
@@ -8,25 +8,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
-
-  private static final String TAG = "MainActivity";
-
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-
-    Toolbar toolbar = findViewById(R.id.toolbar);
-    setSupportActionBar(toolbar);
-  }
-
-  @Override
-  public boolean onOptionsItemSelected(MenuItem item) {
-    if (item.getItemId() == R.id.menu_logout) {
-      Log.d(TAG, "logout clicked");
-      // TODO handle log out
-      return true;
-    }
-    return super.onOptionsItemSelected(item);
-  }
+	
+	private static final String TAG = "MainActivity";
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		Log.d(TAG, "onCreate: called");
+		
+		Toolbar toolbar = findViewById(R.id.toolbar);
+		setSupportActionBar(toolbar);
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if (item.getItemId() == R.id.menu_logout) {
+			Log.d(TAG, "logout clicked");
+			// TODO handle log out
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 }
