2e50b22003f0e0850da94813d9cc9831badfc656
Arnav3094 <arnav3094pvt@gmail.com>
2024-11-26 16:43:40 +0530

    add error handling use SnackbarHelper instead of snackbar better error handling for cases like user already exists, password too short, email incorrect use firebaseManager instead of mAuth

--- app/src/main/java/androidsamples/java/tictactoe/RegisterFragment.java
+++ app/src/main/java/androidsamples/java/tictactoe/RegisterFragment.java
@@ -6,7 +6,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,26 +14,21 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavDirections;
 import androidx.navigation.Navigation;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
 
 public class RegisterFragment extends Fragment {
 
     private static final String TAG = "RegisterFragment";
     private LoginViewModel viewModel;
     private FirebaseManager firebaseManager;
-    private FirebaseAuth mAuth;  @Override
+    
+    @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         firebaseManager = FirebaseManager.getInstance();
-        mAuth = FirebaseAuth.getInstance();
-
         viewModel = new ViewModelProvider(this).get(LoginViewModel.class);
     }
 
@@ -58,58 +52,77 @@ public class RegisterFragment extends Fragment {
                     String confirmPassword = etConfirmPassword.getText().toString().trim();
 
                     if (email.isEmpty() || password.isEmpty() || confirmPassword.isEmpty()) {
-                        Snackbar.make(v, "All fields are required", Snackbar.LENGTH_SHORT).show();
+                        SnackbarHelper.showSnackbar(v, "All fields are required", Snackbar.LENGTH_SHORT, R.color.design_default_color_error);
                         return;
                     }
 
                     if (!password.equals(confirmPassword)) {
-                        Snackbar.make(v, "Passwords do not match", Snackbar.LENGTH_SHORT).show();
+                        SnackbarHelper.showSnackbar(v, "Passwords do not match", Snackbar.LENGTH_SHORT, R.color.design_default_color_error);
                         return;
                     }
-                    mAuth.createUserWithEmailAndPassword(email, password)
-                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                                @Override
-                                public void onComplete(@NonNull Task<AuthResult> task) {
-                                    if (task.isSuccessful()) {
-                                        // Sign in success, update UI with the signed-in user's information
-                                        Log.d(TAG, "createUserWithEmail:success");
-                                        FirebaseUser user = mAuth.getCurrentUser();
-                                        saveUserToRealtimeDatabase(email); // Save to Realtime Database
-                                        Snackbar.make(v, "Registration successful. Please log in.", Snackbar.LENGTH_SHORT).show();
-                                        viewModel.clear();
-                                        NavDirections action = RegisterFragmentDirections.actionRegisterToLogin();
-                                        Navigation.findNavController(v).navigate(action);
-
-                                    } else {
-                                        // If sign in fails, display a message to the user.
-                                        Log.w(TAG, "createUserWithEmail:failure", task.getException());
-                                        Snackbar.make(v, "Registration failed. Please try again.", Snackbar.LENGTH_SHORT).show();
-//                                        updateUI(null);
-                                    }
-                                }
-                            });
+                    firebaseManager.signUp(email, password, new FirebaseManager.OnAuthCompleteListener() {
+                        @Override
+                        public void onSuccess() {
+                            SnackbarHelper.showSnackbar(v, "Registration successful. Please log in.");
+                            viewModel.clear();
+                            NavDirections action = RegisterFragmentDirections.actionRegisterToLogin();
+                            Navigation.findNavController(v).navigate(action);
+                            // TODO: Build view model for registration
+                            // clear the view model
+                        }
+                        
+                        @Override
+                        public void onError(Exception e) {
+                            if(e instanceof FirebaseAuthWeakPasswordException){
+                                Log.w(TAG, "signUp:onError: weak password - " + e.getMessage());
+                                SnackbarHelper.showSnackbar(v, "Password is too weak", Snackbar.LENGTH_LONG, R.color.design_default_color_error);
+                                // clear password
+                                view.findViewById(R.id.edit_password).clearFocus();
+                                view.findViewById(R.id.edit_confirm_password).clearFocus();
+                                
+                                // TODO: clear view model password
+                                
+                                // set focus to enter password
+                                view.findViewById(R.id.edit_password).requestFocus();
+                                
+                            } else if (e instanceof FirebaseAuthInvalidCredentialsException) {
+                                Log.w(TAG, "signUp:onError: invalid email - " + e.getMessage());
+                                SnackbarHelper.showSnackbar(v, "Invalid email", Snackbar.LENGTH_LONG, R.color.design_default_color_error);
+                                // clear email and pass
+                                view.findViewById(R.id.edit_email).clearFocus();
+                                view.findViewById(R.id.edit_password).clearFocus();
+                                view.findViewById(R.id.edit_confirm_password).clearFocus();
+                                
+                                // TODO: clear view model email and password
+                                
+                                // set focus to enter email
+                                view.findViewById(R.id.edit_email).requestFocus();
+                                
+                            } else if (e instanceof FirebaseAuthUserCollisionException) {
+                                Log.w(TAG, "signUp:onError: user already exists - " + e.getMessage());
+                                SnackbarHelper.showSnackbar(v, "User already exists", Snackbar.LENGTH_LONG, R.color.design_default_color_error);
+                                
+                                // clear email and pass
+                                view.findViewById(R.id.edit_email).clearFocus();
+                                view.findViewById(R.id.edit_password).clearFocus();
+                                view.findViewById(R.id.edit_confirm_password).clearFocus();
+                                
+                                // TODO: clear view model email and password
+                                
+                                // set focus to enter email
+                                view.findViewById(R.id.edit_email).requestFocus();
+                                
+                            } else {
+                                Log.e(TAG, "signUp:onError: failed to register", e);
+                                SnackbarHelper.showSnackbar(v, "Failed to register", Snackbar.LENGTH_LONG, R.color.design_default_color_error);
+                                // clear password
+                            }
+                        }
+                    });
 
 
                 });
 
         return view;
-    }
-
-    private void saveUserToRealtimeDatabase(String email) {
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        if (user != null) {
-            String userId = user.getUid();
-            DatabaseReference userRef = FirebaseDatabase.getInstance().getReference("users").child(userId);
-
-            // Save only the email in the Realtime Database
-            userRef.child("email").setValue(email)
-                    .addOnCompleteListener(task -> {
-                        if (task.isSuccessful()) {
-                            Log.d(TAG, "User data saved to Realtime Database");
-                        } else {
-                            Log.e(TAG, "Failed to save user data", task.getException());
-                        }
-                    });
-        }
     }
 }
